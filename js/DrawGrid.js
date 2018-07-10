8530a7d86705d152fb8fa626450b7237aef90156
Barış Köklü <bariskoklu@gmail.com>
2018-07-10 18:53:24 +0300

    played with the positioning of the zones

--- js/DrawGrid.js
+++ js/DrawGrid.js
@@ -33,30 +33,32 @@ function drawPentagonalZone(x1,y1, x2,y2, x3,y3, x4,y4, x5,y5, zoneNumber) {
 function drawGrid() {
 canvasContext.fillText(mouseX + " " + mouseY, mouseX, mouseY); //drawing mouse coordinates on the screen to map out zones
 
-drawQuadrilateralZone(2,205, 72,205, 45,278, 2,278, 1);
-drawQuadrilateralZone(79,205, 190,205, 190,278, 54,278, 2);
-drawQuadrilateralZone(190,205, 322,205, 310,278, 190,278, 3);
-drawQuadrilateralZone(330,205, 400,205, 400,278, 320,278, 4);
-drawQuadrilateralZone(400,205, 471,205, 481,278, 400,278, 5);
-drawQuadrilateralZone(481,205, 602,205, 602,278, 490,278, 6);
-drawQuadrilateralZone(602,205, 724,205, 752,278, 602,278, 7);
-drawQuadrilateralZone(732,205, 798,205, 798,278, 761,278, 8);
-drawPentagonalZone(2,278, 45,278, 40,315, 55,350, 2,350, 9);
-drawPentagonalZone(55,278, 190,278, 190,350, 65,350, 50,315, 10);
+drawQuadrilateralZone(0,203, 72,203, 45,278, 0,278, 1);
+drawQuadrilateralZone(72,203, 190,203, 190,278, 45,278, 0);
+drawQuadrilateralZone(190,203, 322,203, 310,278, 190,278, 3);
+drawQuadrilateralZone(322,203, 400,203, 400,278, 310,278, 4);
+drawQuadrilateralZone(400,203, 480,203, 490,278, 400,278, 5);
+drawQuadrilateralZone(480,203, 602,203, 602,278, 490,278, 6);
+drawQuadrilateralZone(602,203, 730,203, 760,278, 602,278, 7);
+drawQuadrilateralZone(730,203, 800,203, 800,278, 760,278, 8);
+drawPentagonalZone(0,278, 45,278, 40,315, 55,350, 0,350, 9);
+drawPentagonalZone(45,278, 190,278, 190,350, 55,350, 40,315, 10);
 drawQuadrilateralZone(190,278, 310,278, 300,350, 190,350, 11);
-drawQuadrilateralZone(318,278, 400,278, 400,350, 307,350, 12);
-drawQuadrilateralZone(400,278, 481,278, 491,350, 400,350, 13);
+drawQuadrilateralZone(310,278, 400,278, 400,350, 300,350, 12);
+drawQuadrilateralZone(400,278, 490,278, 502,350, 400,350, 13);
 drawQuadrilateralZone(490,278, 602,278, 602,350, 502,350, 14);
-drawPentagonalZone(602,278, 752,278, 755,315, 740,350, 600,350, 15);
-drawPentagonalZone(761,278, 798,278, 798,350, 752,350, 764,318, 16);
-drawPentagonalZone(2,350, 50,350, 115,415, 200,455, 2,455, 17);
-drawQuadrilateralZone(65,350, 190,350, 190,438, 117,400, 18);
-drawQuadrilateralZone(190,350, 300,350, 287,438, 190,438, 19);
-drawQuadrilateralZone(307,350, 491,350, 494,470, 304,470, 20);
-drawQuadrilateralZone(502,350, 602,350, 602,440, 525,460, 21);
-drawQuadrilateralZone(600,350, 740,350, 690,395, 602,440, 22);
-drawPentagonalZone(752,350, 798,350, 798,460, 602,457, 705,410, 23);
-drawQuadrilateralZone(130,430, 310,480, 310,520, 130,520, 24);//labels are off, obviously, haha
-drawQuadrilateralZone(310,480, 494,470, 494,520, 310,520, 25);
-drawQuadrilateralZone(494,470, 680,430, 680,520, 494,520, 26);//labels are off
+drawPentagonalZone(602,278, 760,278, 760,315, 750,350, 602,350, 15);
+drawPentagonalZone(760,278, 800,278, 800,350, 750,350, 760,315, 16);
+drawPentagonalZone(0,350, 50,350, 115,412, 190,450, 0,450, 17);
+drawQuadrilateralZone(50,350, 190,350, 190,450, 115,412, 18);
+drawQuadrilateralZone(190,350, 300,350, 280,468, 190,450, 19);
+//drawQuadrilateralZone(300,350, 502,350, 517,468, 280,468, 20);
+drawPentagonalZone(300,350, 502,350, 517,468, 400,478, 280,468, 20);
+drawQuadrilateralZone(502,350, 602,350, 602,448, 517,468, 21);
+drawQuadrilateralZone(602,350, 750,350, 693,408, 602,448, 22);
+drawPentagonalZone(750,350, 800,350, 800,448, 602,448, 693,408, 23);
+drawQuadrilateralZone(190,450, 280,468, 280,520, 190,520, 24);//labels are off, obviously, haha
+// drawQuadrilateralZone(280,468, 494,470, 494,520, 280,520, 25);
+drawPentagonalZone(280,468, 400,478, 517,468, 517,520, 280,520, 25);
+drawQuadrilateralZone(517,468, 602,448, 602,520, 517,520, 26);//labels are off
 }
