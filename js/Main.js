1d5fb066352635548beca7b22425d5f0cd21db49
McFunkypants <breakdance@mcfunkypants.com>
2018-07-09 20:08:26 -0700

    particles added to game after merge conflicts resolved

--- js/Main.js
+++ js/Main.js
@@ -1,32 +1,32 @@
 var canvas, canvasContext;
 var currentPic = player1;
 
-var character1 = new playerClass(75,75,false);
-var character2 = new playerClass(150,75,true);
+var character1 = new playerClass(75, 75, false);
+var character2 = new playerClass(150, 75, true);
 
 var ballArray = [];
-var ball1 = new ballClass(700,100);
-var ball2 = new ballClass(500,600);
+var ball1 = new ballClass(700, 100);
+var ball2 = new ballClass(500, 600);
 
 
-window.onload = function() {
+window.onload = function () {
 	canvas = document.getElementById('gameCanvas');
 	canvasContext = canvas.getContext('2d');
 	canvas.onmousemove = (evt) => {//gathering mouse coordinates for easy reference during game dev in game play,
-		                             //current use is to outline zones
-		 mouseX = evt.pageX;
-		 mouseY = evt.pageY;
+		//current use is to outline zones
+		mouseX = evt.pageX;
+		mouseY = evt.pageY;
 	}
 
-	colorRect(0,0, canvas.width,canvas.height, 'black');
-	colorText("LOADING IMAGES", canvas.width/2, canvas.height/2, 'white');
+	colorRect(0, 0, canvas.width, canvas.height, 'black');
+	colorText("LOADING IMAGES", canvas.width / 2, canvas.height / 2, 'white');
 
 	loadImages();
 }
 
 function imageLoadingDoneSoStartGame() {
 	var framesPerSecond = 30;
-	setInterval(updateAll, 1000/framesPerSecond);
+	setInterval(updateAll, 1000 / framesPerSecond);
 	setAudioFormat();
 	loadAudios();
 	setupInput();
@@ -34,25 +34,27 @@ function imageLoadingDoneSoStartGame() {
 
 function updateAll() {
 	moveAll();
+	updateAllEmitters(); // see ParticleSystem.js
 	drawAll();
 }
 
 function moveAll() {
 	character1.move();
-  character2.move();
-  moveBalls(ballArray);
+	character2.move();
+	moveBalls(ballArray);
 }
 
 function drawAll() {
 	drawWorld();
-  character1.draw();
-  character2.draw();
+	ParticleRenderer.renderAll(canvasContext); // particle FX
+	character1.draw();
+	character2.draw();
 	drawBallShadows(ballArray);
-  drawBalls(ballArray);
+	drawBalls(ballArray);
 	drawGrid();
 }
 
 
-function drawWorld(){
-  drawBitmapCenteredWithRotation(basketballCourt, canvas.width/2,canvas.height/2,0);
+function drawWorld() {
+	drawBitmapCenteredWithRotation(basketballCourt, canvas.width / 2, canvas.height / 2, 0);
 }
