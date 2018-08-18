d3c388778fff76d387fc22856e81358ab865b1eb
Renaud Marshall <renaud.marshall@gmail.com>
2018-08-17 20:53:11 -0400

    Reduced zone alpha so that court markings are visible
    And added a title tag to index.html

--- js/DrawGrid.js
+++ js/DrawGrid.js
@@ -1,23 +1,32 @@
-var claimColor = [undefined,"#233663","#309c63","#c93038","#51c43f","#417291"];
+var claimColor = [
+    undefined,
+    "#233663",
+    "#309c63",
+    "#c93038",
+    "#51c43f",
+    "#417291"
+];
 
 function drawAnyZone(points, zoneNumber, claimedByPlayerNumber) {
-  canvasContext.strokeStyle = "#383542";
-  canvasContext.beginPath();
-  canvasContext.moveTo(points[0].x, points[0].y);
-  
-  for(let i = 1; i < points.length; i++) {
-    canvasContext.lineTo(points[i].x,points[i].y);
-  }
-  canvasContext.lineTo(points[0].x,points[0].y);
+    canvasContext.globalAlpha = 0.7;
+    canvasContext.strokeStyle = "#383542";
+    canvasContext.beginPath();
+    canvasContext.moveTo(points[0].x, points[0].y);
 
-  canvasContext.stroke();
-  
-  if (claimedByPlayerNumber > 0) {
-    canvasContext.fillStyle = claimColor[claimedByPlayerNumber];
-    canvasContext.fill();
-  }
-  
-  let centerX = (points[1].x - points[0].x)/2 + points[0].x;
-  let centerY = (points[points.length - 1].y - points[0].y)/2 + points[0].y;
-  canvasContextFillStyle = "blue";
+    for (let i = 1; i < points.length; i++) {
+        canvasContext.lineTo(points[i].x, points[i].y);
+    }
+    canvasContext.lineTo(points[0].x, points[0].y);
+
+    canvasContext.stroke();
+
+    if (claimedByPlayerNumber > 0) {
+        canvasContext.fillStyle = claimColor[claimedByPlayerNumber];
+        canvasContext.fill();
+    }
+
+    let centerX = (points[1].x - points[0].x) / 2 + points[0].x;
+    let centerY = (points[points.length - 1].y - points[0].y) / 2 + points[0].y;
+    canvasContextFillStyle = "blue";
+    canvasContext.globalAlpha = 1;
 }
